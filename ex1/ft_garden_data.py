bc4257aac787aa3904cc3f569e8ebdb4d04ed19b
Heron Laoutaris <hlaoutaris@gmail.com>
2026-03-25 17:01:14 +0100

    updated

--- ex1/ft_garden_data.py
+++ ex1/ft_garden_data.py
@@ -1,29 +1,51 @@
-# @property = decorator
-# ! range() allowed: why
+# ! For each plant, the class will be instantiated
+# ! then the attributes will be set to their specific values.
+
 class Plant:
-    def __init__(self, name: str, height: int, age: int):
+    def __init__(self: "Plant", name: str, height: str, age: str) -> None:
         self._name = name
         self._height = height
         self._age = age
 
     @property
-    def name(self) -> str:
-        return self._name.capitalize()
+    def name(self: "Plant") -> str:
+        return self._name.capitalize() + ": "
 
     @property
-    def height(self) -> str:
-        return f"{self._height}cm"
+    def height(self: "Plant") -> str:
+        return self._height + "cm, "
 
     @property
-    def age(self) -> str:
-        return f"{self._age} days old"
+    def age(self: "Plant") -> str:
+        return self._age + " days old"
+
+    def show(self: "Plant") -> None:
+        print(self.name + self.height + self.age)
 
 
 if __name__ == "__main__":
-    rose = Plant("rose", 25, 30)
-    sunflower = Plant("sunflower", 80, 45)
-    cactus = Plant("cactus", 15, 120)
+    rose = Plant("rose", "25", "30")
+    sunflower = Plant("sunflower", "80", "45")
+    cactus = Plant("cactus", "15", "120")
 
-    plants = [rose, sunflower, cactus]
-    for plant in plants:
-        print(f"{plant.name}:",  f"{plant.height},", plant.age)
+    rose.show()
+    sunflower.show()
+    cactus.show()
+
+# class Plant:
+#     def __init__(self, name: str, height: int, age: int) -> None:
+#         self._name = name
+#         self._height = height
+#         self._age = age
+
+#     @property
+#     def name(self) -> str:
+#         return self._name.capitalize()
+
+#     @property
+#     def height(self) -> str:
+#         return self._height + "cm"
+
+#     @property
+#     def age(self) -> str:
+#         return self._age + "days old"
